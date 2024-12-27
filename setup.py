3188f1e5a5a7ed302ccef18453f3331d1a910680
jesjack <jesjack25_03@hotmail.com>
2024-12-26 22:41:12 -0600

    acomoda un poco todo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiuncensored",
-    version="1.3",
+    version="1.4",
     description="Cliente para interactuar con el modelo llama-3-70b y la API AIUncensored",
     author="Vauth",
     url="https://github.com/jesjack/aiuncensored",
